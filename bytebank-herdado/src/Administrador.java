5277fa569dd34498eca62d2a0d74f117c5df66b8
Heverton Reis <heverton.reis04@gmail.com>
2022-12-28 21:13:43 -0300

    doc: Finalizando curso de herança, polimorfismo e interfaces

--- bytebank-herdado/src/Administrador.java
+++ bytebank-herdado/src/Administrador.java
@@ -1,26 +1,24 @@
-
 public class Administrador extends Funcionario implements Autenticavel {
 
-	private int senha;
+	private AutenticacaoUtil autenticador;
 
-	@Override
-	public double getBonificacao() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
+    public Administrador(){
+        this.autenticador = new AutenticacaoUtil();
+    }
 
-	@Override
+    @Override
     public void setSenha(int senha) {
-        this.senha = senha;
+        this.autenticador.setSenha(senha);
     }
 
     @Override
     public boolean autentica(int senha) {
-        if (this.senha == senha) {
-            return true;
-        } else {
-            return false;
-        }
+        return this.autenticador.autentica(senha);
+    }
+
+    @Override
+    public double getBonificacao() {
+        return super.getSalario() + 100;
     }
 
 }
